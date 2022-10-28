e99e30760c466ebaabb47c4a7120bfbddbd44f91
Taism Chaniev <taismdg@icloud.com>
2022-10-28 19:11:52 +0200

    28okt

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -59,7 +59,7 @@ export default function Footer() {
         </div>
     </div>
     <div className="bg-black py-6 px-4 bg-gray-900 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2022 <a href="https://flowbite.com/">Flowbite™</a>. All Rights Reserved.
+        <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2022 <a href="https://flowbite.com/">Taism™</a>. Web Development.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center md:mt-0">
             <a href="#" className="text-gray-400 hover:text-gray-900 dark:hover:text-white">
