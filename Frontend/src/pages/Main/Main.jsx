c5dcca74650798917ee056342e4479ab703b10b3
Taism Chaniev <taismdg@icloud.com>
2022-11-06 20:20:46 +0100

    modal

--- Frontend/src/pages/Main/Main.jsx
+++ Frontend/src/pages/Main/Main.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Navbar, Footer } from '../../components';
+import { Navbar, Footer, Request } from '../../components';
 import { Home, Registration, Login } from "../";
 
 const Main = () => {
+
+  // const modal = true;
+  const modal = false;
+
   return (
     <>
+    {modal && <Request /> }
       <div className="main">
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/ingconstructie-registration" element={<Registration/>} />
-          <Route path="/login" element={<Login/>} />
-        </Routes>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="/ingconstructie-registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/request" element={<Request />} />
+          </Routes>
       </div>
-    <div className="footer">
-      <Footer />
-    </div>
-  </>
+
+      <div className="footer">
+            <Footer />
+      </div>
+    </>
   );
 }
 
