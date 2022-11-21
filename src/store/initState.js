1d5484a1210bc37af7f4155b3d25557eab57eeff
Taism Chaniev <taismdg@icloud.com>
2022-11-21 16:10:02 +0100

    ggg

--- src/store/initState.js
+++ src/store/initState.js
@@ -30,28 +30,28 @@ export const initState = {
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
     {
-      id: 1,
+      id: 5,
       name: 'Tegels plaatsen',
       info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       imageSrc: './Tegelsleggen.jpeg',
       imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
     {
-      id: 2,
+      id: 6,
       name: 'Schilderwerken',
       info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       imageSrc: './Schilderwerken.jpeg',
       imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
     },
     {
-      id: 3,
+      id: 7,
       name: 'Parket leggen',
       info: ' lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       imageSrc: './Parketleggen.jpeg',
       imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
     },
     {
-      id: 4,
+      id: 8,
       name: 'Installatie van deuren',
       info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       imageSrc: './InstallatieVanDeDeur.jpeg',
@@ -67,17 +67,17 @@ export const initState = {
     {
       id: 1,
       title: 'Slide 1',
-      src: 'http://localhost:3000/3.jpg',
+      src: 'http://localhost:3000/elektro.jpg',
     },
     {
       id: 2,
       title: 'Slide 2',
-      src: 'http://localhost:3000/2.jpg',
+      src: 'http://localhost:3000/Tegelsleggen.jpeg',
     },
     {
       id: 3,
       title: 'Slide 3',
-      src: 'http://localhost:3000/6.jpg',
+      src: 'http://localhost:3000/Ontmanteling.jpeg',
     },
   ],
 
